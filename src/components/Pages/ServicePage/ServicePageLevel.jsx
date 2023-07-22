fb8849dd277023e87f71ad44e6130f1ee4e7537c
rishab-sudo <rishabdutt4@gmail.com>
2023-07-22 14:51:24 +0530

    22july

--- src/components/Pages/ServicePage/ServicePageLevel.jsx
+++ src/components/Pages/ServicePage/ServicePageLevel.jsx
@@ -2,39 +2,114 @@ import { Box, Flex, Heading, Image, Text, useColorModeValue } from "@chakra-ui/r
 import { BiWalletAlt, BiAddToQueue } from "react-icons/bi"
 import { MdOutlineHighQuality } from "react-icons/md"
 import { AiOutlineDash } from "react-icons/ai"
+import {Container,Row,Col} from "react-bootstrap"
 export const ServicePageLevel = () => {
 
     return (
+        <>
     <Box textAlign={"left"} marginLeft={"5%"} marginBottom={"80px"} marginTop={"50px"}>
         <Box m="1vw" mb="2vw"><Heading fontSize={{ base: "5vw", sm: "5vw", md: "3.5vw", lg: "3.5vw", xl: "3.5vw" }}>LET'S <span style={{ color: "#fe611d" }}>LEVEL UP</span></Heading></Box>
         <Box m="1vw"><Heading fontSize={{ base: "5vw", sm: "5vw", md: "3.5vw", lg: "3.5vw", xl: "3.5vw" }}><span style={{ color: "#fe611d" }}>YOUR BUSINESS</span> <span>WITH US</span></Heading></Box>
         <br />
-        <Box w={{ base: "80%", sm: "75%", md: "65%", lg: "65%", xl: "65%" }}>
+        </Box>
+
+        <Container>
+            <Row>
+                <Col xlg={8} lg={8} md={12} sm={11} xs={11}>
+
+<div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+
+<div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",marginBottom:"auto"}}>
+<Box borderRadius={"full"} backgroundColor={useColorModeValue("#fef1e8", "#121212")} width={"5vw"} height={"5vw"} display={"flex"} justifyContent={"center"} alignItems={"center"} >
+<BiWalletAlt color="#e6b829" style={{width:"4vw",height:"4vw"}}/>
+</Box>
+<div style={{fontSize:"1.2vw",textTransform:"capitalize",fontWeight:"500"}}>
+<p>creativity</p>  
+</div>
+</div>
+<div style={{marginTop:"-30px"}}>
+<Box display={useColorModeValue('inline', "none")}><Image src="vectorb.png" alt="vector" /></Box>
+<Box display={useColorModeValue('none', "inline")}><Image src="vector.png" alt="vector" /></Box>
+</div>
+
+<div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",marginBottom:"auto"}}>
+<Box borderRadius={"full"} backgroundColor={useColorModeValue("#fef1e8", "#121212")} width={"5vw"} height={"5vw"} display={"flex"} justifyContent={"center"} alignItems={"center"} >
+<BiWalletAlt color="#e6b829" style={{width:"4vw",height:"4vw",}}/>
+</Box>
+<div style={{fontSize:"1.2vw",textTransform:"capitalize",fontWeight:"500"}}>
+<p>quantity over<br/>quality</p>  
+</div>
+</div>
+<div style={{marginTop:"-30px"}}>
+<Box display={useColorModeValue('inline', "none")}><Image src="vectorb.png" alt="vector" /></Box>
+<Box display={useColorModeValue('none', "inline")}><Image src="vector.png" alt="vector" /></Box>
+</div>
+
+<div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",marginBottom:"auto"}}>
+<Box borderRadius={"full"} backgroundColor={useColorModeValue("#fef1e8", "#121212")} width={"5vw"} height={"5vw"} display={"flex"} justifyContent={"center"} alignItems={"center"} >
+<BiWalletAlt color="#e6b829" style={{width:"4vw",height:"4vw"}}/>
+</Box>
+<div style={{fontSize:"1.2vw",textTransform:"capitalize",fontWeight:"500"}}>
+<p>available in <br/>7 city</p>  
+</div>
+</div>
+<div style={{marginTop:"-30px",}}>
+<Box display={useColorModeValue('inline', "none")}><Image src="vectorb.png" alt="vector" /></Box>
+<Box display={useColorModeValue('none', "inline")}><Image src="vector.png" alt="vector" /></Box>
+</div>
+
+<div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",marginBottom:"auto"}}>
+<Box borderRadius={"full"} backgroundColor={useColorModeValue("#fef1e8", "#121212")} width={"5vw"} height={"5vw"} display={"flex"} justifyContent={"center"} alignItems={"center"} >
+<BiWalletAlt color="#e6b829" style={{width:"4vw",height:"4vw"}}/>
+</Box>
+<div style={{fontSize:"1.2vw",textTransform:"capitalize",fontWeight:"500"}}>
+<p>increased productivity</p>  
+</div>
+</div>
+
+</div>
+                </Col>
+
+
+
+
+
+
+
+                </Row>
+
+        </Container>
+        </>
+    )
+}
+
+{/* <Box w={{ base: "90%", sm: "87%", md: "90%", lg: "57%", xl: "57%" }}>
             <Flex justifyContent={"space-around"} alignItems={"center"}>
-                {/* AiOutlineDash */}
+              
 
-                <Box display={"flex"} flexDirection={"column"} justifyContent={"center"}  alignItems={"center"} mt={{ base: "10px", sm: "1px", md: "1px", lg: "1px", xl: "1px" }}>
+                <Box display={"flex"} flexDirection={"column"} justifyContent={"space-around"}  alignItems={"center"} mt={{ base: "10px", sm: "1px", md: "1px", lg: "1px", xl: "1px" }}>
                     <Box borderRadius={"full"} backgroundColor={useColorModeValue("#fef1e8", "#121212")}
-                        padding={"10px"} fontSize={{ base: "5vw", sm: "5vw", md: "3.5vw", lg: "3vw", xl: "3vw" }} ><BiWalletAlt color="#e6b829" size={"4vw"} /></Box>
-                    <Box><Text fontSize={{ base: "2.5vw", sm: "2.5vw", md: "1.2vw", lg: "1.2vw", xl: "1.2vw" }} fontWeight={"500"} m="0px" textAlign={"center"}>Creativity</Text></Box>
+                        padding={"10px"} fontSize={{ base: "5vw", sm: "5vw", md: "3.5vw", lg: "3vw", xl: "3vw" }} >
+                    <BiWalletAlt color="#e6b829" size={"4vw"} /></Box>
+                    <Box><Text fontSize={{ base: "2vw", sm: "2vw", md: "2vw", lg: "1.2vw", xl: "1.2vw" }} fontWeight={"500"} mt="0px" textAlign={"center"}>Creativity</Text></Box>
                 </Box>
 
                 <Box display={useColorModeValue('inline', "none")}><Image src="vectorb.png" alt="vector" /></Box>
                 <Box display={useColorModeValue('none', "inline")}><Image src="vector.png" alt="vector" /></Box>
 
 
-                <Box display={"flex"} flexDirection={"column"} alignItems={"center"} mt={{ base: "10px", sm: "1px", md: "1px", lg: "1px", xl: "1px" }}>
+                <Box display={"flex"} flexDirection={"column"} justifyContent={"space-around"} alignItems={"center"} mt={{ base: "10px", sm: "1px", md: "1px", lg: "1px", xl: "1px" }}>
                     <Box borderRadius={"full"} backgroundColor={useColorModeValue("#fef1e8", "#121212")} padding={"10px"} fontSize={{ base: "5vw", sm: "5vw", md: "3.5vw", lg: "3vw", xl: "3vw" }}><MdOutlineHighQuality size={"4vw"} color="#e6b829" /></Box>
-                    <Box><Text fontSize={{ base: "2.5vw", sm: "2.5vw", md: "1.2vw", lg: "1.2vw", xl: "1.2vw" }} fontWeight={"500"} m="0px" textAlign={"center"}>Quality over Quantity</Text></Box>
+                    <Box><Text fontSize={{ base: "2vw", sm: "2vw", md: "1.2vw", lg: "1.2vw", xl: "1.2vw" }} fontWeight={"500"} m="0px" textAlign={"center"}>Quality over Quantity</Text></Box>
                 </Box>
                 <Box display={useColorModeValue('inline', "none")}><Image src="vectorb.png" alt="vector" /></Box>
                 <Box display={useColorModeValue('none', "inline")}><Image src="vector.png" alt="vector" /></Box>
 
 
 
-                <Box display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"}  >
+                <Box display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"space-around"}  >
                     <Box borderRadius={"full"} backgroundColor={useColorModeValue("#fef1e8", "#121212")} padding={"10px"} fontSize={{ base: "5vw", sm: "5vw", md: "3.5vw", lg: "3vw", xl: "3vw" }}><BiAddToQueue color="#e6b829" size={"4vw"} /></Box>
-                    <Box textAlign={"center"}><Text fontSize={{ base: "2.5vw", sm: "2.5vw", md: "1.2vw", lg: "1.2vw", xl: "1.2vw" }} fontWeight={"500"} m="0px"  textAlign={"center"}>Available in 7 City</Text></Box>
+                    <Box textAlign={"center"}><Text fontSize={{ base: "2vw", sm: "2vw", md: "1.2vw", lg: "1.2vw", xl: "1.2vw" }} fontWeight={"500"} m="0px"  textAlign={"center"}>Available in 7 City</Text></Box>
 
                 </Box>
 
@@ -42,12 +117,12 @@ export const ServicePageLevel = () => {
                 <Box display={useColorModeValue('none', "inline")}><Image src="vector.png" alt="vector" /></Box>
 
 
-                <Box display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"center"} >
+                <Box display={"flex"} flexDirection={"column"} alignItems={"center"} justifyContent={"space-around"} >
                     <Box borderRadius={"full"} backgroundColor={useColorModeValue("#fef1e8", "#121212")} padding={"10px"} fontSize={{ base: "5vw", sm: "5vw", md: "3.5vw", lg: "3vw", xl: "3vw" }}><BiWalletAlt color="#e6b829" size={"4vw"}/></Box>
-                    <Box textAlign={"center"}><Text width="100%" fontSize={{ base: "2.5vw", sm: "2.5vw", md: "1.2vw", lg: "1.2vw", xl: "1.2vw" }} fontWeight={"500"} m="0px">Increased Productivity</Text></Box>
+                    <Box textAlign={"center"}><Text width="100%" fontSize={{ base: "2vw", sm: "2vw", md: "1.2vw", lg: "1.2vw", xl: "1.2vw" }} fontWeight={"500"} mt="0px">Increased Productivity</Text></Box>
                 </Box>
 
             </Flex>
-        </Box>
-    </Box>)
-}
+        </Box> */}
+  
+    
--- src/components/Pages/ServicePage/ServicePageTypes.jsx
+++ src/components/Pages/ServicePage/ServicePageTypes.jsx
@@ -9,56 +9,60 @@ export const ServicePageTypes = () => {
 
     return (
         <>
+        <section id="service">
             {/* left-side-content */}
             <Container fluid>
                 <Row style={{marginTop:"20px"}}>
-                    <Col lg={7}>
+                    <Col lg={8}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"1px 10rem 10rem 1px"}>
                         <div className="service_content_div">
                             <div className="service_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>social media management</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}>Are you looking to increase brand awareness, generate leads, or establish thought leadership in your industry?<br />
+                                <p className="Scard_para">Are you looking to increase brand awareness, generate leads, or establish thought leadership in your industry?<br />
                                     Look no further than Reclame Hub, our Social media management services in Noida, India Elevate your Brand’s Online Presence, to connect you with your target audience and strong online presence.
                                 </p>
                             </div>
                             <div className="service_img_div">
-                                <img className="service_img" src="gif6.gif" />
+                                <img className="service_img" src="s1.gif" />
                             </div>
-
                         </div>
+</Box>
                     </Col>
                 </Row>
         
                 <Row style={{marginTop:"3%"}}>
-                    <Col lg={7}>
+                    <Col lg={8}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"1px 10rem 10rem 1px"}>
                         <div className="service_content_div">
                             <div className="service_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>content marketing</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}>Discover the best B2B Content Marketing Agency Services in India with Reclame Hub. From blog posts and whitepapers to social media 
+                                <p className="Scard_para">Discover the best B2B Content Marketing Agency Services in India with Reclame Hub. From blog posts and whitepapers to social media 
                                 content and email marketing campaigns, we have the expertise and resources to deliver results that exceed your expectations.
 
                                 </p>
                             </div>
                             <div className="service_img_div">
-                                <img className="service_img" src="gif6.gif" />
+                                <img className="service_img" src="s2.gif" />
                             </div>
-
                         </div>
+</Box>
                     </Col>
                 </Row>
         
                 <Row style={{marginTop:"3%"}}>
-                    <Col lg={7}>
+                    <Col lg={8}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"1px 10rem 10rem 1px"}>
                         <div className="service_content_div">
                             <div className="service_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>online reputation management</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}>Our Reliable Marketing services in Noida, India, specializes in delivering effective marketing solutions tailored to your specific needs. Our team looks into the necessity of having a strong online presence to successfully manage brand reputation.
+                                <p className="Scard_para">Our Reliable Marketing services in Noida, India, specializes in delivering effective marketing solutions tailored to your specific needs. Our team looks into the necessity of having a strong online presence to successfully manage brand reputation.
                                 </p>
                             </div>
                             <div className="service_img_div">
-                                <img className="service_img" src="gif6.gif" />
+                                <img className="service_img" src="s3.gif" />
                             </div>
-
                         </div>
+</Box>
                     </Col>
                 </Row>
             </Container>
@@ -66,150 +70,161 @@ export const ServicePageTypes = () => {
             {/* right-side-content */}
             <Container fluid style={{ display: "flex", justifyContent: "flex-end", alignItems: "flex-end",marginTop:"2%" }}>
                 <Row className="row justify-content-end" >
-                    <Col lg={7} >
+                    <Col lg={8} >
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"10rem 1px 1px 10rem"}>
                         <div className="Rservice_content_div">
                             <div className="Rservice_img_div">
-                                <img className="Rservice_img"src="gif6.gif" />
+                                <img className="Rservice_img"src="s4.gif" />
                             </div>
                             <div className="Rservice_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>graphic designing</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}>Expert Graphic Designing Company in Noida | Creative Design Services | Reclame Hub
+                                <p className="Scard_para">Expert Graphic Designing Company in Noida | Creative Design Services | Reclame Hub
                                 </p>
                             </div>
-
                         </div>
+                        </Box>
                     </Col>
 
-                    <Col lg={7} style={{marginTop:"3%"}}>
+                    <Col lg={8} style={{marginTop:"3%"}}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"10rem 1px 1px 10rem"}>
                         <div className="Rservice_content_div">
                             <div className="Rservice_img_div">
-                                <img className="Rservice_img"src="gif6.gif" />
+                                <img className="Rservice_img"src="s5.gif" />
                             </div>
                             <div className="Rservice_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>WEB DESIGN & DEVELOPMENT</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}>Professional Website Development Services | Reclame Hub Leading Website Development Company offering custom ecommerce solutions. 
+                                <p className="Scard_para">Professional Website Development Services | Reclame Hub Leading Website Development Company offering custom ecommerce solutions. 
                                 Expertise in Magento, WordPress Website Development in India. Upgrade your business today.
                                 </p>
                             </div>
-
                         </div>
+                        </Box>
                     </Col>
                 </Row>
             </Container>
             {/* left-side-content */}
             <Container fluid style={{marginTop:"2%"}}>
                 <Row >
-                    <Col lg={7}>
+                    <Col lg={8}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"1px 10rem 10rem 1px"}>
                         <div className="service_content_div">
                             <div className="service_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>digital advertisement </h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}> Elevate your small business with Reclame Hub, a premier digital marketing agency specializing in tailored strategies to enhance your online presence, 
+                                <p className="Scard_para"> Elevate your small business with Reclame Hub, a premier digital marketing agency specializing in tailored strategies to enhance your online presence, 
                                 drive traffic, and boost conversions.
                                 </p>
                             </div>
                             <div className="service_img_div">
-                                <img className="service_img" src="gif6.gif" />
+                                <img className="service_img" src="s6.gif" />
                             </div>
-
                         </div>
+                        </Box>
+
                     </Col>
                 </Row>
             </Container>
             <Container fluid>
                 <Row style={{marginTop:"3%"}}>
-                    <Col lg={7}>
+                    <Col lg={8}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"1px 10rem 10rem 1px"}>
                         <div className="service_content_div">
                             <div className="service_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>video - ad film</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}>At Reclame Hub, we enhance your virtual world with the power of captivating video content. Our skilled team will create high-quality video-ad films 
+                                <p className="Scard_para">At Reclame Hub, we enhance your virtual world with the power of captivating video content. Our skilled team will create high-quality video-ad films 
                                 that effectively communicate your brand message, engage your target audience, and leave a lasting impact.
                                 </p>
                             </div>
                             <div className="service_img_div">
-                                <img className="service_img" src="gif6.gif" />
+                                <img className="service_img" src="s7.gif" />
                             </div>
 
                         </div>
-
+                        </Box>
                     </Col>
                 </Row >
             </Container>
             <Container fluid>
                 <Row style={{marginTop:"3%"}}>
-                    <Col lg={7}>
+                    <Col lg={8}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"1px 10rem 10rem 1px"}>
                         <div className="service_content_div">
                             <div className="service_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>email marketing</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}> As experts in email marketing, we know that effective communication is key. With our tailored strategies and services, we help build strong connections with your audience. Our team will craft personalized email 
+                                <p className="Scard_para"> As experts in email marketing, we know that effective communication is key. With our tailored strategies and services, we help build strong connections with your audience. Our team will craft personalized email 
                                 campaigns that deliver the right message to people at the right time.
                                 </p>
                             </div>
                             <div className="service_img_div">
-                                <img className="service_img" src="gif6.gif" />
+                                <img className="service_img" src="s8.gif" />
                             </div>
-
                         </div>
+                        </Box>
                     </Col>
                 </Row>
             </Container>
             {/* right-side-content */}
               <Container fluid style={{ display: "flex", justifyContent: "flex-end", alignItems: "flex-end",marginTop:"2%" }}>
                 <Row className="row justify-content-end">
-                    <Col lg={7}>
+                    <Col lg={8}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"10rem 1px 1px 10rem"}>
                         <div className="Rservice_content_div">
                             <div className="Rservice_img_div">
-                                <img className="Rservice_img"src="gif6.gif" />
+                                <img className="Rservice_img"src="s99.gif" />
                             </div>
                             <div className="Rservice_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>seo development</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}>In today’s landscape enhancing online visibility is of utmost importance. Reclame Hub offers top-level SEO services to your website to rank higher. We provide the best possible solutions to build your
+                                <p className="Scard_para">In today’s landscape enhancing online visibility is of utmost importance. Reclame Hub offers top-level SEO services to your website to rank higher. We provide the best possible solutions to build your
                                  online presence and drive more traffic to your website.
 
                                 </p>
                             </div>
-
                         </div>
+                        </Box>
                     </Col >
 
-                    <Col lg={7} style={{marginTop:"3%"}}>
+                    <Col lg={8} style={{marginTop:"3%"}}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"10rem 1px 1px 10rem"}>
                         <div className="Rservice_content_div">
                             <div className="Rservice_img_div">
                                 <img className="Rservice_img"src="gif6.gif" />
                             </div>
                             <div className="Rservice_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>campaign marketing</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}>We work closely with our expertly crafted campaigns to develop tailored strategies that align with your business goals, from identifying your target audience and creating engaging content to driving measurable results.
+                                <p className="Scard_para">We work closely with our expertly crafted campaigns to develop tailored strategies that align with your business goals, from identifying your target audience and creating engaging content to driving measurable results.
 
                                 </p>
                             </div>
-
                         </div>
+                        </Box>
                     </Col>
                 </Row>
             </Container>
             {/* left-side */}
             <Container fluid>
                 <Row style={{marginTop:"3%"}}>
-                    <Col lg={7}>
+                    <Col lg={8}>
+                    <Box   fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"1px 10rem 10rem 1px"}>
                         <div className="service_content_div">
                             <div className="service_heading_div">
                                 <h2 style={{ textTransform: "uppercase", fontWeight: "700",fontSize:"2vw",marginBottom:"1.5rem" }}>APP DESIGN & DEVELOPMENT</h2>
-                                <p style={{ fontWeight: "600", textTransform: "capitalize" }}> Reclame Hub specializes in app design and development, crafting intuitive and visually appealing applications that deliver outstanding user experiences. 
+                                <p className="Scard_para"> Reclame Hub specializes in app design and development, crafting intuitive and visually appealing applications that deliver outstanding user experiences. 
                                 we'll bring your ideas to life, ensuring seamless functionality and a polished interface.
                            </p>
                             </div>
                             <div className="service_img_div">
-                                <img className="service_img" src="gif6.gif" />
+                                <img className="service_img" src="s11.gif" />
                             </div>
-
                         </div>
+</Box>
                     </Col>
                 </Row>
                 </Container>
 
+                </section>
 
-
-
+                </>
+    )
+}
 
             {/* <Box w={{ base: "100%", sm: "80%", md: "80%", lg: "80%", xl: "80%" }} p="2vw" fontFamily={"poppins"} boxShadow={"xl"} bg={useColorModeValue('white', '#3D3A3A')} borderRadius={"1px 10rem 10rem 1px"}>
                 <Flex gap="10%" >
@@ -475,6 +490,3 @@ export const ServicePageTypes = () => {
 
             <Box borderTop="1px solid white" height={"2px"} marginTop={"50px"} marginBottom={"50px"}></Box> */}
 
-        </>
-    )
-}
