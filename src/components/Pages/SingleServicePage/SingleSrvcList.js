500e9f0e2bfb567039bf30bb26437ebaff6160ca
rishab-sudo <rishabdutt4@gmail.com>
2023-07-31 20:08:22 +0530

    31july

--- src/components/Pages/SingleServicePage/SingleSrvcList.js
+++ src/components/Pages/SingleServicePage/SingleSrvcList.js
@@ -9,7 +9,7 @@ const SingleSrvcTable = (props) => {
     <>
     <Container>
     <div className='table_main_heading'>
-    <p>here comes <span style={{color:"#ff7722"}}>our services...</span></p>
+    <p>here comes <span style={{color:"#ff7721"}}>our services...</span></p>
     </div>
     </Container>
      
@@ -17,9 +17,11 @@ const SingleSrvcTable = (props) => {
      <main className='main_div' >
  <div >
     <p className='table_title'>{props.title0}</p>
+    <Box color= {useColorModeValue('#000', "#15E888")} >
     <p>{props.heading0}</p>
+    </Box>
    </div>
-   <Box color= {useColorModeValue('#FF7722', "#FFB800")} >
+   <Box color= {useColorModeValue('#FF7721', "#FFB800")} >
    <div >
     <p>{props.text0}</p>
    </div>
@@ -29,40 +31,56 @@ const SingleSrvcTable = (props) => {
    <main className='main_div'style={{marginBottom:'auto'}} >
    <div >
   <p className='table_title'>{props.title1}</p>
+  <Box color= {useColorModeValue('#000', "#15E888")} >
     <p>{props.heading1}</p>
+    </Box>
    </div>
-   <div style={{color:'red',}}>
+   <Box color= {useColorModeValue('#FF7721', "#FFB800")} >
+   <div >
     <p>{props.text1}</p>
    </div>
+   </Box>
    </main>
     
     <main className='main_div'style={{marginBottom:'auto'}} >
     <div >
   <p className='table_title'>{props.title2}</p>
+  <Box color= {useColorModeValue('#000', "#15E888")} >
     <p>{props.heading2}</p>
+    </Box>
    </div>
-   <div style={{color:'red',}}>
+   <Box color= {useColorModeValue('#FF7721', "#FFB800")} >
+   <div >
     <p>{props.text2}</p>
    </div>
+   </Box>
    </main>
     
     <main className='main_div'style={{marginBottom:'auto',}} >
     <div >
   <p className='table_title'>{props.title3}</p>
-    <p >{props.heading3}</p>
+  <Box color= {useColorModeValue('#000', "#15E888")} >
+    <p>{props.heading3}</p>
+    </Box>
    </div>
-   <div style={{color:'red',}}>
+   <Box color= {useColorModeValue('#FF7721', "#FFB800")} >
+   <div >
     <p>{props.text3}</p>
    </div>
+   </Box>
    </main>
     <main className='main_div'style={{marginBottom:'auto'}} >
     <div >
   <p className='table_title'>{props.title4}</p>
+  <Box color= {useColorModeValue('#000', "#15E888")} >
     <p>{props.heading4}</p>
+    </Box>
    </div>
-  <div style={{color:'red',}}>
+   <Box color= {useColorModeValue('#FF7721', "#FFB800")} >
+   <div >
     <p>{props.text4}</p>
    </div>
+   </Box>
    </main>
     </Container>
     </>
