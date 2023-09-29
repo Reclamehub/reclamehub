f4807cb94093719b674c604cb1fe1d2cc85410d1
rishab-sudo <rishabdutt4@gmail.com>
2023-09-29 16:41:34 +0530

    28sep

--- src/components/Pages/ExpServiceTypes/IsoCertificate.js
+++ src/components/Pages/ExpServiceTypes/IsoCertificate.js
@@ -13,16 +13,19 @@ const IsoCertificate = () => {
       <ExpServiceBanner heading="Claim the Best ISO Certification for Your Business" text="Apply the Best ISO Certification for your business. Our professionals at Reclame Hub Will Guide you and take care of the entire ISO Certification process of until you receive the Certification." img="expiso.png"/>
     </Container>
 
-  <Container className='d-flex flex-column justify-content-start align-items-start'>
-        <div className='exp_iso_heading_div'>
+<Container fluid className='box-shadow'>
+  <Container className='d-flex flex-column justify-content-start align-items-start inner_box '>
+        <div className='exp_iso_heading_div '>
             <heading className="">What is ISO?</heading>
         </div>
         <div className='exp_iso_text_div'>
             <text>International Standardization Organization (ISO) was founded in 1947. Its main objective is to create guidelines and standards for different types of industries. Because no one could tell which product was good for the customer. Thus,  ISO Came into existence and made some international standards for different kinds of products and services so the customer can trust the quality. Its main purpose is to maintain Quality in the international marketplace. ISO also make guidelines for some industries so all the international organization will work on the same format. It makes it easy to coordinate with local authorities and customers.</text>
         </div>
     </Container>
+    </Container>
 
-    <Container className='d-flex flex-column justify-content-start align-items-start'>
+    <Container fluid className='box-shadow'>
+    <Container className='d-flex flex-column justify-content-start align-items-start inner_box'>
         <div className='exp_iso_heading_div'>
             <heading className="">What is IAF ?</heading>
         </div>
@@ -30,8 +33,10 @@ const IsoCertificate = () => {
             <text>International Accreditation Forum (IAF) is an organization that brings accreditation bodies from different countries and regions. An ISO Certification endorsed by an IAF member holds significant international recognition. It can take your organization's product/service to the next level. Also, ISO Certificates approved by IAF members are required for many government tenders. It can increase the chances of getting that tender. </text>
         </div>
     </Container>
+    </Container>
 
-    <Container className='d-flex flex-column justify-content-start align-items-start'>
+<Container fluid className='box-shadow'>
+    <Container className='d-flex flex-column justify-content-start align-items-start inner_box'>
         <div className='exp_iso_heading_div'>
             <heading className="">Why is ISO Certification Important? </heading>
         </div>
@@ -43,8 +48,10 @@ const IsoCertificate = () => {
            </ul>
         </div>
     </Container>
+    </Container>
 {/*  */}
-<Container className='d-flex flex-column justify-content-start align-items-start'>
+<Container fluid className='box-shadow'>
+<Container className='d-flex flex-column justify-content-start align-items-start inner_box'>
         <div className='exp_iso_heading_div'>
             <heading className="">How Does ISO Certification Help Businesses to Grow?</heading>
         </div>
@@ -54,8 +61,10 @@ ISO certification can avoid many potential legal issues because each country has
 </text>
         </div>
     </Container>
+    </Container>
 
-    <Container className='d-flex flex-column justify-content-start align-items-start'>
+    <Container fluid className='box-shadow'>
+    <Container className='d-flex flex-column justify-content-start align-items-start inner_box'>
         <div className='exp_iso_heading_div'>
             <heading className="">Which ISO Certificate Is Best for You? </heading>
         </div>
@@ -65,8 +74,10 @@ ISO certification can avoid many potential legal issues because each country has
              </text>
         </div>
     </Container>
+    </Container>
 
-    <Container className='diff_category_container d-flex flex-column justify-content-start align-items-start'>
+    <Container fluid className='box-shadow'>
+    <Container className='diff_category_container d-flex flex-column justify-content-start align-items-start inner_box'>
         <div className='exp_iso_heading_div'>
             <heading className="">Different Categories of ISO Certification </heading>
         </div>
@@ -81,20 +92,20 @@ ISO certification can avoid many potential legal issues because each country has
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Environmental Management:</p>
+            <p className='ptag_heading'>Environmental Management:</p>
             <p>  ISO 14001 - Uses for Environmental Management Systems</p>
             <p>  ISO 14000 - Uses for Environmental Management - Overview and Principles</p>
             <p>  ISO 14004 - Uses for Environmental Management Systems - General Guidelines  </p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Information Security:</p>
+            <p className='ptag_heading'> Information Security:</p>
             <p> ISO/IEC 27001 - Uses for Information Security Management Systems</p>
             <p> ISO/IEC 27002 - Uses for Code of Practice for Information Security controls</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Occupational Health and Safety:</p>
+            <p className='ptag_heading'>Occupational Health and Safety:</p>
             <p> ISO 45001 - Uses for Occupational Health and Safety Management Systems</p>
             </div>
 
@@ -104,85 +115,85 @@ ISO certification can avoid many potential legal issues because each country has
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Energy Management:</p>
+            <p className='ptag_heading'>Energy Management:</p>
             <p> ISO 50001 - Uses for Energy Management Systems</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Risk Management:</p>
+            <p className='ptag_heading'>Risk Management:</p>
             <p> ISO 31000 - Uses for Risk Management - Principles and Guidelines</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Medical Devices: </p>
+            <p className='ptag_heading'>Medical Devices: </p>
             <p>ISO 13485 - Uses for Medical Devices - Quality Management Systems</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Automotive:</p>
+            <p className='ptag_heading'>Automotive:</p>
             <p> ISO 26262 - Uses for Functional Safety for Road Vehicles</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Aerospace:</p>
+            <p className='ptag_heading'>Aerospace:</p>
             <p> ISO 26262 - Uses for Functional Safety for Road Vehicles</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>IT and Software:</p>
+            <p className='ptag_heading'>IT and Software:</p>
             <p> ISO/IEC 20000 - Uses for IT Service Management</p>
             <p>ISO/IEC 12207 - Uses for Software Life Cycle Processes</p>
             <p>ISO/IEC 27018 - Uses for Protection of Personally Identifiable Information in Public Clouds</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Social Responsibility:</p>
+            <p className='ptag_heading'>Social Responsibility:</p>
             <p>ISO 26000 - Uses for Guidance on Social Responsibility</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Energy Efficiency:</p>
+            <p className='ptag_heading'>Energy Efficiency:</p>
             <p> ISO 50002 - Uses for Energy Audits. </p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Supply Chain Management:</p>
+            <p className='ptag_heading'>Supply Chain Management:</p>
             <p> ISO 28000 - Uses for Specification for Security Management Systems for the Supply Chain</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Country Codes:</p>
+            <p className='ptag_heading'>Country Codes:</p>
             <p> ISO 3166 - Uses for Codes for the Names of Countries</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Language and Script Codes:</p>
+            <p className='ptag_heading'>Language and Script Codes:</p>
             <p> ISO 639 -  Uses for Codes for the Representation of Names of Languages </p>
             <p>ISO 15924 - Codes for the Representation of Names of Scripts</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Paper Sizes:</p>
+            <p className='ptag_heading'>Paper Sizes:</p>
             <p> ISO 216 - Uses for Paper Sizes</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Photography:</p>
+            <p className='ptag_heading'>Photography:</p>
             <p> ISO 12232 - Uses for Digital Still Camera Image Quality</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Quality of Water:</p>
+            <p className='ptag_heading'>Quality of Water:</p>
             <p> ISO 5667 - Uses for Water Quality Sampling</p>
             </div>
 
             <div className='exp_iso_pTag_div'>
-            <p>Currencies:</p>
+            <p className='ptag_heading'>Currencies:</p>
             <p>ISO 4217 - Uses for Codes for the Representation of Currencies</p>
             </div>
        
     </Container>
-
+</Container>
    
 
     </Box>
